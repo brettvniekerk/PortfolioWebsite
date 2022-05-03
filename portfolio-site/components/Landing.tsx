ea7a2227fe4ae796af3edc736a768b046dd09e78
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-05-04 01:46:49 +1000

    Basic design implemented

--- portfolio-site/components/Landing.tsx
+++ portfolio-site/components/Landing.tsx
@@ -1,6 +1,24 @@
+import styles from '../styles/Landing.module.scss'
+import { useState, useEffect } from 'react'
+
 const Landing = () => {
+
+  // state and effect
+  const [underscoreOn, setUnderscoreOn] = useState(true) // state for if underscore showing
+
+  useEffect(() => { // runs on each render
+    const timer = setTimeout(() => { // creates a timer that runs every 500 milliseconds
+      setUnderscoreOn(!underscoreOn) // inverts state
+    }, 500)
+
+    return () => clearTimeout(timer) // clears timer if component unmounted
+  })
+
   return (
-    <div>Landing</div>
+    <div className={styles['landing-page']}>
+      <h1 className={styles['title']}>Hi{ underscoreOn && <span className={styles['underscore']}> |</span> }</h1> {/* shows blinking | (used to be underscore but cbs to change state name) -- if emoji use  &#128075; */}
+      <h2 className={styles['subtitle']}>My name's Brett, and I wanna be your next front end developer!</h2>
+    </div>
   )
 }
 
--- portfolio-site/pages/index.tsx
+++ portfolio-site/pages/index.tsx
@@ -1,26 +1,29 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
 import Landing from '../components/Landing'
+import Scroll from '../components/Scroll'
 import About from '../components/About'
 import Projects from '../components/Projects'
 import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>PORTFOLIO | BRETT VAN NIEKERK</title>
         <meta name="description" content="This is my portfolio website! Take some time to look around :)" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* persists through out page */}
       <Landing />
+
+      {/* scrolling right side elements */}
+      <Scroll />
       <About />
       <Projects />
 
-      <Footer />
-
+      {/* maybe <Footer /> */}
     </div>
   )
 }
