8e9fc7a8748aae063a104aa5cb82733272be4818
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-05-04 21:25:28 +1000

    Some changes

--- portfolio-site/components/Scroll.tsx
+++ portfolio-site/components/Scroll.tsx
@@ -1,6 +1,11 @@
+import styles from '../styles/Scroll.module.scss'
+
 const Scroll = () => {
   return (
-    <div>Scroll</div>
+    <div className={styles['scroll-section']}>
+      <div className={styles['scroll']}>SCROLL <br />
+      &#8595;</div>
+    </div>
   )
 }
 
