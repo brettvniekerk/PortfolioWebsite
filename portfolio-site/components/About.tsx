58d11d25066d1aaf133c6093bf61455b75568991
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-05-12 06:21:16 +1000

    about section done for now -- scuffed way of doing URL construction but I really like it

--- portfolio-site/components/About.tsx
+++ portfolio-site/components/About.tsx
@@ -1,31 +1,54 @@
 import styles from '../styles/About.module.scss'
+import { RefObject, useRef } from 'react'
 
 const About = () => {
+
+  const gRef = useRef<HTMLButtonElement>(null) // github button ref
+  const lRef = useRef<HTMLButtonElement>(null) // linkedin button ref
+
+  const onClickButton = (buttonPressed: RefObject<HTMLButtonElement>) => {
+    window.open(`https://www.${buttonPressed.current?.innerText.toLowerCase()}.com/${buttonPressed.current?.innerText.toLowerCase() === 'linkedin' ? 'in/brett-van-niekerk-2a9799236' : 'brettvniekerk'}`) // scuffed way of constructing URL but i like it
+  }
+
   return (
     <div className={styles['about-section']}>
       <div className={styles['about-box']}>
+
+
+
         <h1 className={styles['section-title']}>ABOUT ME</h1>
-        <br />
         <p className={styles['section-p']}>&emsp;{`I'm currently looking for some full-time, front-end development work. I believe I can offer your team enthusiasm, energy, a strong work ethic, and a willingness to learn new things!`}</p>
-        <br />
         <p className={styles['section-p']}>&emsp;{`I'm passionate about bringing designs to life on the web, and have exercised these interests by building skills in HTML, CSS, and various JavaScript frameworks.`}</p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+
+        <br className={styles['break']} />
+
         <h1 className={styles['section-title']}>MY SKILLS</h1>
+        <p className={styles['section-p']}>Core Skills:</p>
         <ul>
           <li className={styles['list-item']}>Node frameworks such as React, Express, and Next</li>
           <li className={styles['list-item']}>JavaScript, HTML5, CSS3</li>
           <li className={styles['list-item']}>SCSS, and Sass</li>
+          <li className={styles['list-item']}>Typescript</li>
         </ul>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <p className={styles['section-p']}>Familiar Skills:</p>
+        <ul>
+          <li className={styles['list-item']}>Git</li>
+          <li className={styles['list-item']}>Python</li>
+        </ul>
+
+        <br className={styles['break']} />
+
         <h1 className={styles['section-title']}>MY LINKS</h1>
+        
+        <br className={styles['break']} />
+
+        <div className={styles['social-span']}>
+          <button className={styles['social-button']} onClick={ () => onClickButton(gRef) } ref={ gRef }>GITHUB</button> {/* pass ref as arg to onclick func */}
+          <button className={styles['social-button']} onClick={ () => onClickButton(lRef) } ref={ lRef }>LINKEDIN</button> {/* pass ref as arg to onclick func */}
+        </div>
+
+
+
       </div>
     </div>
   )
